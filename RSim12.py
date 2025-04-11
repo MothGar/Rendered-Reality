fb63f6f3999ba5c417a0fa3e8911a70486d534e9
MothGar <tgarner@asu.edu>
2025-04-10 18:47:54 -0700

    Update RSim12.py

--- RSim12.py
+++ RSim12.py
@@ -603,17 +603,7 @@ Chladni Mode: {"ON" if use_chladni else "OFF"}
 - Y: r={st.session_state.r_y}, l={st.session_state.l_y}
 - Z: r={st.session_state.r_z}, l={st.session_state.l_z}
 """)
-x = np.linspace(-domain_scale / 2, domain_scale / 2, grid_size)
-y = np.linspace(-domain_scale / 2, domain_scale / 2, grid_size)
-z = np.linspace(-domain_scale / 2, domain_scale / 2, grid_size)
 
-X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
-
-if 'Z' in locals():
-    spiral_twist_rad = np.radians(spiral_twist_rate * Z)
-    EX = np.sin(fx * X + phase_x + spiral_twist_rad)
-    EY = np.sin(fy * Y + phase_y + spiral_twist_rad)
-    EZ = np.sin(fz * Z + phase_z + spiral_twist_rad)
 interference = np.abs(EX * EY * EZ)
 
 field_norm = (interference - interference.min()) / (interference.max() - interference.min())
